154a7c87223250621b0f9a230c7c48cff89f74bb
BenaGachiri <gachiribena@gmail.com>
2023-02-15 11:52:52 +0300

    added calculator functions

--- calculator.py
+++ calculator.py
@@ -1,9 +1,35 @@
 #this is a calculator project
 
-# number1=eval(input("enter first number:"))
-# number2=eval(input("enter second number:"))
+num1=eval(input("enter first number:"))
+num2=eval(input("enter second number:"))
 
-# operator=input("enter operator:")
-def add (num1+num2)
-result =num1+number 
-return result
+operator=input("enter operator:")
+def add (num1,num2):
+    result=num1+num2 
+    print (result)
+
+def subtract(num1,num2): 
+    result=num1-num2
+    print (result)
+
+def divide(num1,num2):
+    result=num1/num2
+    print (result)
+
+def mulplication(num1,num2):
+    result=num1*num2
+    print (result)  
+
+if operator=="+":    
+    add(num1,num2)
+elif operator=="/":
+    subtract(num1,num2) 
+elif operator=="-":
+    divide(num1,num2)
+elif operator=="*":
+    mulplication(num1,num2) 
+    
+elif operator =="X" or operator == "x" or operator=="*":
+    mulplication(num1,num2)
+else:
+    print("invalid input")
